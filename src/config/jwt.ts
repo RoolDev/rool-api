752a4582a1e0141ad72d598d971bf23f1c53baa2
Lucas Dias <lucas.dias@avenuecode.com>
2020-03-28 01:49:28 -0300

    fix: Fixed JWT expiry time

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -5,6 +5,6 @@ const { JWT_SECRET, JWT_EXPIRE } = process.env;
 export default {
   secret: JWT_SECRET,
   signOptions: {
-    expiresIn: JWT_EXPIRE,
+    expiresIn: +JWT_EXPIRE,
   },
 } as JwtModuleOptions;
