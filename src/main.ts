b5508589b1f11efb8cf83f7f64633e9b566bfabc
Lucas Dias <lucas.dias@avenuecode.com>
2020-03-28 01:29:06 -0300

    fix: Overrided ValidationPipe error

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, BadRequestException } from '@nestjs/common';
+import { ValidationError } from 'class-validator';
 
 import dotenvFlow = require('dotenv-flow');
 
@@ -8,7 +9,17 @@ async function bootstrap() {
   dotenvFlow.config();
 
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+  // Override ValidationPipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      validationError: {
+        target: false,
+      },
+      exceptionFactory: (errors: ValidationError[]) =>
+        new BadRequestException(errors),
+    }),
+  );
 
   await app.listen(process.env.PORT || 3000);
 }
